eb79dfcaebe8106ec43dc9ea6acc88015431baca
Craig Condon <craig@spiceapps.com>
2013-10-17 23:20:02 -0700

    cleanup

---
+++ public/js/models/index.js
@@ -0,0 +1,7 @@
+module.exports = function (mojo) {
+
+  // TODO 
+  mojo.models.set({
+    user: undefined, // new User()
+  });
+}
--- public/js/plugins.js
+++ public/js/plugins.js
@@ -6,11 +6,12 @@ mojo          = require("mojojs");
  * template plugins
  */
 
-paperclip.use(require("./paper/modifiers"));
-paperclip.use(require("./paper/bindings"));
+paperclip.use(require("./paper/modifiers")); // template modifiers
+paperclip.use(require("./paper/bindings"));  // custom DOM bindings
 
 /**
  * mojo plugins
  */
 
-mojo.use(require("./commands"));
+mojo.use(require("./commands")); // register global commands
+mojo.use(require("./models"));   // attach to the models singleton
