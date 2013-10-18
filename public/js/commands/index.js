4f82fa1a41c7c0419c4e368a60958131d4116144
Craig Condon <craig@spiceapps.com>
2013-10-17 23:23:41 -0700

    cleanup

--- public/js/commands/index.js
+++ public/js/commands/index.js
@@ -1,3 +1,17 @@
+var funwrap = require("funwrap");
+
+var commands = [
+  require("./initialize"),
+  require("./load"),
+  require("./bootstrap")
+];
+
 module.exports = function (mojo) {
-  //TODO
+
+  var mediator = mojo.mediator = funwrap().mediator;
+
+  for(var i = commands.length; i--;) {
+    commands[i](mediator);
+  }
+
 }
