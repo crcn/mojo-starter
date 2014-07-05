cc0969f5cf31d4f3cd207a03fd3739d0ecbfa932
Craig Condon <craig@spiceapps.com>
2014-07-05 09:31:30 -0700

    upgrade

--- app/js/index.js
+++ app/js/index.js
@@ -1,31 +1,18 @@
 var Application = require("mojo-application");
 
 module.exports = Application.extend({
-  registerPlugins: function () {
+  plugins: [
 
+    require("mojo-mediator"),
+    require("mojo-views"),
+    require("mojo-paperclip"),
 
-    // globally accessible functions
-    this.use(require("mojo-mediator"));
-
-    // model - TODO
-    // this.use(require("mojo-models"))
-
-    // view controller
-    this.use(require("mojo-views"))
-
-    // view
-    this.use(require("mojo-paperclip"));
-
-    // routes - TODO
-    // this.use(require("mojo-router"));
-
-
-    this.use(require("./commands"));
-    this.use(require("./models"));
-    this.use(require("./views"));
-    this.use(require("./templates"));
-    this.use(require("./routes"));
-  },
+    require("./commands"),
+    require("./models"),
+    require("./views"),
+    require("./templates"),
+    require("./routes")
+  ],
   didInitialize: function (options) {
     $(options.element).append(this.views.create("main").render());
   }
