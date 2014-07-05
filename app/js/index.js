a2fd8fd76db01e123150dad4621d5bfecf826f14
Craig Condon <craig@spiceapps.com>
2014-07-05 09:36:54 -0700

    start on router

--- app/js/index.js
+++ app/js/index.js
@@ -4,14 +4,16 @@ module.exports = Application.extend({
   plugins: [
 
     require("mojo-mediator"),
+    require("mojo-router"),
+    require("mojo-models"),
     require("mojo-views"),
     require("mojo-paperclip"),
 
     require("./commands"),
+    require("./routes"),
     require("./models"),
     require("./views"),
-    require("./templates"),
-    require("./routes")
+    require("./templates")
   ],
   didInitialize: function (options) {
     $(options.element).append(this.views.create("main").render());
